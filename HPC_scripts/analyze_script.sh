2f1fb2d5fb297b92852612341f9c86fcc9274c37
esmall2023 <86076632+esmall2023@users.noreply.github.com>
2022-06-09 08:05:45 -0400

    Update analyze_script.sh

--- HPC_scripts/analyze_script.sh
+++ HPC_scripts/analyze_script.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#$ -cwd
-#$ -j y
-#$ -S /bin/bash
-#$ -M esmall2@bowdoin.edu -m be
-source /mnt/local/python-venv/dlc/bin/activate
-export DLClight="True";
-python3.6 '/mnt/research/hhorch/esmall2/practice_DLC/actual_demov2-ELS-2022-06-02/analyze_new_video.py'
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+source /mnt/local/python-venv/dlc-2.2/bin/activate
+export DLClight="True"; 
+python 'full/filepath/to/your/video/videos'
 
 
