da5cae3eb1a5cc7c7d6fd343ea3c167a8db4aa8b
esmall2023 <86076632+esmall2023@users.noreply.github.com>
2022-06-09 08:06:36 -0400

    Update analyze_video.py

--- HPC_scripts/analyze_video.py
+++ HPC_scripts/analyze_video.py
@@ -1,7 +1,7 @@
 import deeplabcut
 
-config_path = '/mnt/research/hhorch/esmall2/practice_DLC/actual_demov2-ELS-2022-06-02/config.yaml'
-deeplabcut.analyze_videos(config_path,['/mnt/research/hhorch/esmall2/practice_DLC/videos/']
-save_as_csv=True, videotype='avi')
+config_path = '/file/path/to/config.yaml'
+deeplabcut.analyze_videos(config_path,['/file/path/to/videos/']
+save_as_csv=True)
 
 
