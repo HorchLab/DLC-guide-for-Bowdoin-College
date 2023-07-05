18af71fb108e557d2cb00d2e8a67f90ae27cabcc
Tom Han <chan@bowdoin.edu>
2023-07-05 14:43:40 -0400

    Get filtering process to work and generate all pdfs.

--- R files/1_Master_Filter.R
+++ R files/1_Master_Filter.R
@@ -2,16 +2,11 @@ library(tidyverse)
 library(MASS)
 library(stringr)
 source("R files/utils.R")
+# This script is designed in a way that you can change all useful parameters
+# before the for loop. Don't touch stuff below unless u know what's happening. 
 
-#### 1_Master.R 
-### This script controls the process of going from a DLC .csv file to a graphical .pdf file.
-### We do this in 5 steps, each with its script. See the tutorial video and handout for more guidance.
 
-formatted_time <- format(Sys.time(), "%Y-%m-%d-%H-%M")
-output_description <- ""
-### STEP 0: Define your directories
 primary_directory <- "~/summer2023/DLC-guide-for-Bowdoin-College"
-output_directory <- paste0("graphs_output/", formatted_time, output_description)
 input_directory <- "DLC_output/DLC_csv_files_it7_s4_stim01"
 output_directory <- "filtering_output"
 setwd(primary_directory)
@@ -24,39 +19,40 @@ if (!file.exists(output_directory)) {
   cat("Output directory already exists.\n")
 }
 
-### STEP 1: Decide on your filename(s)
+# Change this if you want to display different audio. 
+# REMEMBER to change the output name too!!!
+filtering_critiera <- function() {
+  # for S12 use !(info_vec[5] %in% c("S11","G","U")), cuz naming method for s12 is ass. 
+  (info_vec[6] == "F") && (info_vec[5] == "U")
+}
+output_name <- "U_F_stim01"  # don't add .pdf here. 
+
 file_list <- list.files(path = input_directory, pattern='.csv') # make sure to identify which directory 
-stripped_files <- str_remove(file_list, pattern='.csv')
-cat("# of .csv files found: ", length(stripped_files), "\n")
-results = matrix(NA, nrow = length(stripped_files), ncol = 2)
-colnames(results) = c("File name", "Average turn (degrees)")
+cat("# of .csv files found: ", length(file_list), "\n")
 num.files = 1
 
 ### STEP 2: Load in the functions
 source("R files/2_Functions.R") # this will read in the script containing the necessary functions
 
-pdf(paste0(output_directory, "/", "S12_M_stim01.pdf"), width = 8.5, height = 11)
-par(mfrow = c(6,1), mar = c(1,1,2,0.5), oma = c(1,1,1,1),cex.lab=1,cex.axis=1, 
-    cex.main = 1)
-
 filter_plot <- function(filename) {
   plot(shots, average_angle_per_shot, main = convert_to_title(file_name), xlab = "",
-  ylab = "Angle (degrees)", col = "blue", type = "l", ylim = c(-25,25))
+       ylab = "Angle (degrees)", col = "blue", type = "l", ylim = c(-25,25), xaxt = "n")
   grid() # add gridlines
   abline(h = 0, col = "red") # add a red line at y = 0
 }
 
+pdf(paste0(output_directory, "/", output_name, ".pdf"), width = 8.5, height = 11)
+par(mfrow = c(6,1), mar = c(1,1,2,0.5), oma = c(1,1,1,1),cex.lab=1,cex.axis=1, 
+    cex.main = 1)
 ## This loop allows us to read every csv file in a directory
-for (i in stripped_files) {
-  file_name = i # this is one file in a directory
-  file_name_csv <- paste(file_name, ".csv", sep='') 
-  output_name <- NULL
-  # print(file_name_csv)
+for (i in file_list) {
+  file_name = i # this is one file in a directory, now with ".csv"
   
   info_vec <- convert_to_datavec(file_name)
   
-  if ((info_vec[6] == "M") && (!(info_vec[5] %in% c("S11","G","U")))) {
-    # Filtering and only plot those that fits in the selection. 
+  # Filtering and only plot those that fits in the selection. 
+  if (filtering_critiera()) {
+    
     print(convert_to_title(file_name))
     
     ### STEP 3: Load in the data
@@ -65,18 +61,12 @@ for (i in stripped_files) {
     ### STEP 4: Perform the relevant calculations
     minimum_sound <- 0 # in dB, typically zero 
     maximum_sound <- 90 # in dB, depends on experiment
-    tick_mark_interval <- 10 # in dB, space between tick marks
     
     #results[num.files, 3] = sd.turn
     source("R files/4_Calculator_Fix_Anchor.R")
-    #cat("Step 4 Complete. Calculations performed.", "\n")
-  
+    
     filter_plot()
     
-    ### STEP 5: Generate and save the cricket's graph
-    # source("R files/5_Grapher_Fixed_Anchor.R")
-    #cat("Step 5 Complete. Graph saved as:", output_name_pdf ,"\n")
-     
     num.files = num.files + 1
   }
 }
