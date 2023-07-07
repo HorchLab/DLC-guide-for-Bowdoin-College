4cfcae3b37cbb672334783cfe63a6e03a95f1b92
Tom Han <chan@bowdoin.edu>
2023-07-06 23:52:04 -0400

    minor changes on filter

--- R files/1_Master_Filter.R
+++ R files/1_Master_Filter.R
@@ -18,14 +18,14 @@ if (!file.exists(output_directory)) {
   cat("Output directory already exists.\n")
 }
 
-# Change this if you want to display different audio. 
+# Change this if you want to display different audio.
 # REMEMBER to change the output name too!!!
 # filtering_criteria <- function() {
   # for S12 use !(info_vec[5] %in% c("S11","G","U")), cuz naming method for s12 is ass. 
   # (info_vec[6] == "F") && (info_vec[5] == "S11")
   # (as.numeric(format(as.Date(info_vec[1]), "%m")) >= 6) && (format(as.Date(info_vec[1]), "%Y") == 2021)
 # }
-output_name <- "S11_stim01"  # don't add .pdf here. 
+output_name <- "S11_stim01"  # don't add .pdf here.
 
 criterias <- list(function() {(info_vec[5] == "S11")},
                   function() {(info_vec[5] == "U")}, 
@@ -35,7 +35,7 @@ criteria_order <- c("S11", "U", "GFP", "S12")
 
 file_list <- list.files(path = input_directory, pattern='.csv') # make sure to identify which directory 
 cat("# of .csv files found: ", length(file_list), "\n")
-num.files = 1
+num_files <- 1
 
 ### STEP 2: Load in the functions
 source("R files/2_Functions.R") # this will read in the script containing the necessary functions
@@ -45,7 +45,7 @@ filter_plot <- function(filename) {
        ylab = "Angle (degrees)", col = "mediumorchid4", type = "l", ylim = c(-25,25), xaxt = "n")
   grid() # add gridlines
   abline(h = 0, col = "red") # add a red line at y = 0
-  
+
   plot(right_leg_center_dist, type = "l", col = "blue", ylim = c(-100, 100),
        main = paste("leg-center distance for", convert_to_title(file_name)),
        xlab = "", ylab = "Distance(px)")
@@ -65,24 +65,23 @@ for (filtering_criteria in criterias) {
       cex.main = 1.5)
   n <- n + 1
   for (file_name in file_list) {
-  
     info_vec <- convert_to_datavec(file_name)
-    
-    # Filtering and only plot those that fits in the selection. 
+
+    # Filtering and only plot those that fits in the selection.
     if (filtering_criteria()) {
       print(convert_to_title(file_name))
       ### STEP 3: Load in the data
       source("R files/3_Reader.R")
       
       ### STEP 4: Perform the relevant calculations
-      minimum_sound <- 0 # in dB, typically zero 
+      minimum_sound <- 0 # in dB, typically zero
       maximum_sound <- 90 # in dB, depends on experiment
       
       source("R files/4_Calculator_More_Angle.R")
       
       filter_plot()
       
-      num.files = num.files + 1
+      num_files <- num_files + 1
     }
   }
   dev.off()
